01b2066342356e0d245292cc4622a9d06c37b7d0
ueken65 <ueken6522@gmail.com>
2021-05-05 14:42:34 +0900

    refactor: コンポーネントの責務を分割

--- src/components/MahjongTable/TileImages.tsx
+++ src/components/MahjongTable/TileImages.tsx
@@ -2,32 +2,29 @@ import React from "react";
 import Tile from "~/domain/models/Tile";
 import styles from "./MahjongTable.module.css";
 
-interface Props {
+type Props = {
   tiles: Tile[];
   onClick: (event: any) => void;
-}
-
-const TileImages = (props: Props) => {
-  const { tiles, onClick } = props;
-
-  // TODO: 4枚引かれたら disable っぽくする
-  return (
-    <>
-      {tiles.map((tile, i) => {
-        return (
-          <img
-            className={styles.tile_image}
-            src={tile.getImagePath}
-            key={i}
-            data-index={i}
-            data-tile-number={tile.getTileNumber}
-            data-suit={tile.getSuit}
-            onClick={onClick}
-          />
-        );
-      })}
-    </>
-  );
 };
 
-export default TileImages;
+const Component: React.FC<Props> = (props) => (
+  <div>
+    {props.tiles.map((tile, i) => {
+      return (
+        <img
+          className={styles.tile_image}
+          src={tile.getImagePath}
+          key={i}
+          data-index={i}
+          data-tile-number={tile.getTileNumber}
+          data-suit={tile.getSuit}
+          onClick={props.onClick}
+        />
+      );
+    })}
+  </div>
+);
+
+Component.displayName = "TileImages";
+
+export default Component;
